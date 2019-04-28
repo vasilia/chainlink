e1dd9797741805008c8f61ee79896fc031182b1d
Steve Ellis <email@steveell.is>
2019-04-28 14:07:53 -0400

    WIP: routing unit test

--- core/web/box_test.go
+++ core/web/box_test.go
@@ -33,12 +33,17 @@ func TestBox_MatchWildcardBoxPath(t *testing.T) {
 	assert.Equal(
 		t,
 		jobSpecRunShowRouteInfo,
-		web.MatchWildcardBoxPath(boxList, "/job_specs/abc123/runs/abc123", "routeInfo.json"),
+		web.MatchWildcardBoxPath(boxList, "/jobs/abc123/runs/abc123", "routeInfo.json"),
 	)
 	assert.Equal(
 		t,
 		"",
-		web.MatchWildcardBoxPath(boxList, "/job_specs/abc123/runs/abc123", "notFound.json"),
+		web.MatchWildcardBoxPath(boxList, "/jobs/abc123/runs/abc123", "notFound.json"),
+	)
+	assert.Equal(
+		t,
+		jobSpecRunShowRouteInfo,
+		web.MatchWildcardBoxPath(boxList, "/jobs/1be8102f144945ce946a3d478524da40/runs/id/9e8e1dc70d0f49b6a9335b769c1d33d8", "notFound.json"),
 	)
 }
 
